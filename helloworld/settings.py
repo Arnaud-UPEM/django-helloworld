a03ea9af3d30798176e7428a939f5537ada734c4
Arnaud-UPEM <54961057+Arnaud-UPEM@users.noreply.github.com>
2020-01-28 14:42:32 -0400

    Update settings.py

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -75,10 +75,7 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'helloworld.db'),
-    }
+    'default': {}
 }
 
 
